f1017e80f3ec1c5a75776d710d73eb5eb75d36d8
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-01 16:14:54 +0300

    Edited app.js

--- 01.Asynchronous_Programming/01.XmlHttpRequest/scripts/app.js
+++ 01.Asynchronous_Programming/01.XmlHttpRequest/scripts/app.js
@@ -1,3 +1,14 @@
 function loadRepos() {
-   console.log("TODO...");
+  const div = document.getElementById("res");
+
+  let url = "https://api.github.com/users/testnakov/repos";
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", function () {
+    if (request.readyState == 4 && request.status == 200) {
+      div.textContent = request.responseText;
+    }
+  });
+  request.open("GET", url);
+  request.send()
 }
