3a418476108031f9a9ee539dc0e618019262dfc3
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-03 13:45:19 +0300

    Edited app.js

--- 01.Asynchronous_Programming/03.githubCommits/app.js
+++ 01.Asynchronous_Programming/03.githubCommits/app.js
@@ -1,4 +1,32 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const fields = {
+    username: document.getElementById("username"),
+    repo: document.getElementById("repo"),
+  };
+
+  const list = {
+    commits: document.getElementById("commits"),
+  };
+
+  const url = `https://api.github.com/repos/${fields.username.value}/${fields.repo.value}/commits`;
+  list.commits.innerHTML = "";
+
+  try {
+    const response = await fetch(url);
+
+    if (response.ok == false) {
+      throw new Error(`(${response.status}) ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    for (let repo of data) {
+      let li = document.createElement("li");
+      li.innerText = `${repo.commit.author.name}: ${repo.commit.message}`;
+
+      list.commits.appendChild(li);
+    }
+  } catch (error) {
+    list.commits.innerHTML = error;
+  }
 }
