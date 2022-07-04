e4069440f005ffebd548ac32d8bc5df67bdb8630
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-04 14:01:18 +0300

    Edited app.js

--- 02.Exercise_Asynchronous_Programming/01.busStop/app.js
+++ 02.Exercise_Asynchronous_Programming/01.busStop/app.js
@@ -1,3 +1,26 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const inputField = document.querySelector("#stopId");
+  const stopNameDiv = document.querySelector("#stopName");
+  const bussesUl = document.querySelector("#buses");
+
+  try {
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${inputField.value}`;
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+      throw new Error(`Error: Invalid Id!`);
+    }
+    const busesInfo = await response.json();
+
+    stopNameDiv.innerText = busesInfo.name;
+    bussesUl.innerHTML = "";
+
+    Object.entries(busesInfo.buses).forEach((b) => {
+      let li = document.createElement("li");
+      li.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+      bussesUl.appendChild(li);
+    });
+  } catch (error) {
+    stopNameDiv.innerHTML = `<h4 style="color:red;">${error.message}</h4>`;
+  }
 }
