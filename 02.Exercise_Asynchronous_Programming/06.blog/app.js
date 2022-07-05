4352321aa7daa9424910cd0fc708bc008d943628
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-05 23:05:20 +0300

    Edited app.js

--- 02.Exercise_Asynchronous_Programming/06.blog/app.js
+++ 02.Exercise_Asynchronous_Programming/06.blog/app.js
@@ -13,27 +13,26 @@ function attachEvents() {
   };
 
   const url = {
-    posts: "http://localhost:3030/jsonstore/blog/posts",
+    posts: "http://localhost:3030/jsonstore/blog/posts/",
     comments: "http://localhost:3030/jsonstore/blog/comments",
   };
 
   //   Add functionality to the main buttons
   button.load.addEventListener("click", async () => {
-    const data = await fetch(url.posts);
-    const response = await data.json();
+    const response = await fetch(url.posts);
+    const data = await response.json();
 
-    Object.entries(response).forEach((e) => showAllBlogs(e, button.select));
+    Object.entries(data).forEach((e) => showAllBlogs(e, button.select));
+  });
+
+  button.view.addEventListener("click", () => {
+    showSingleBlog(button, url, post);
+    getPostComments(button, url, post);
   });
 }
 
 attachEvents();
 
-function showAllBlogs(blog, select) {
-  select.appendChild(createNewElement("option", "", blog[1].title, blog[1].id));
-
-}
-
-// Factory func for creating a new elements
 function createNewElement(type, className, data, value) {
   let element = document.createElement(type);
 
@@ -50,4 +49,38 @@ function createNewElement(type, className, data, value) {
   }
 
   return element;
+}
+
+function showAllBlogs(blog, select) {
+  select.appendChild(createNewElement("option", "", blog[1].title, blog[1].id));
+}
+
+async function showSingleBlog(button, url, post) {
+  const currentPostId = button.select.value;
+  const response = await fetch(url.posts + currentPostId);
+  const data = await response.json();
+
+  post.title.textContent = data.title;
+  post.body.textContent = data.body;
+}
+
+async function getPostComments(button, url, post) {
+  const currentPostId = button.select.value;
+  const commentsRes = await fetch(url.comments);
+  const commentData = await commentsRes.json();
+
+  const postComments = Object.values(commentData).filter(
+    (c) => c.postId === currentPostId
+  );
+  addPostComments(postComments, post);
+}
+
+function addPostComments(comments, post) {
+  post.comment.innerHTML = "";
+
+  comments.forEach((c) => {
+    const li = createNewElement("li");
+    li.textContent = c.text;
+    post.comment.appendChild(li);
+  });
 }
