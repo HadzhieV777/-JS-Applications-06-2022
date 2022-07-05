71a128f00a3a50cf437be2e6cefbbe95b6a1e12b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-05 12:11:56 +0300

    Edited app.js

--- 02.Exercise_Asynchronous_Programming/03.forecaster/app.js
+++ 02.Exercise_Asynchronous_Programming/03.forecaster/app.js
@@ -10,25 +10,42 @@ function attachEvents() {
   };
 
   const locationsUrl = "http://localhost:3030/jsonstore/forecaster/locations";
-  // upcoming: `http://localhost:3030/jsonstore/forecaster/upcoming/${upcomingLocation}`,
 
   // Add functionality to the button
   getButton.addEventListener("click", async (e) => {
+    const symbol = {
+      Sunny: "☀",
+      "Partly sunny": "⛅",
+      Overcast: "☁",
+      Rain: "☂",
+      degrees: "°",
+    };
+
     const data = await fetch(locationsUrl);
     const response = await data.json();
 
     const currentCity = response.find((c) => c.name == inputField.value);
 
     div.forecast.style.display = "block";
-    getCurrentConditions(currentCity, div.current);
-    inputField.value = ''
+    inputField.value = "";
+
+    try {
+      if (currentCity == undefined) {
+        throw new Error(`Invalid Location!`);
+      }
+      getCurrentConditions(currentCity, div.current, symbol);
+      getUpcommingConditions(currentCity, div.upcoming, symbol);
+    } catch (e) {
+      div.current.appendChild(createNewElement("h2", "error", e));
+    }
   });
 }
 
 attachEvents();
 
+// Factory func for creating a new elements
 function createNewElement(type, className, data) {
-    let element = document.createElement(type);
+  let element = document.createElement(type);
 
   if (className) {
     element.className = className;
@@ -41,43 +58,78 @@ function createNewElement(type, className, data) {
   return element;
 }
 
-async function getCurrentConditions(location, section) {
+// Func that sends a request and process data for current condition section
+async function getCurrentConditions(location, section, symbol) {
   const todayUrl = `http://localhost:3030/jsonstore/forecaster/today/${location.code}`;
 
   const data = await fetch(todayUrl);
   const response = await data.json();
 
-  buildCurrentCondition(response, section);
+  buildCurrentCondition(response, section, symbol);
 }
 
-function buildCurrentCondition(location, section) {
-  const symbol = {
-    Sunny: "☀",
-    "Partly sunny": "⛅",
-    Overcast: "☁",
-    Rain: "☂",
-    degrees: "°",
-  };
+// Func that sends a request and process data for upcomming section
+async function getUpcommingConditions(location, section, symbol) {
+  const upcommingUrl = `http://localhost:3030/jsonstore/forecaster/upcoming/${location.code}`;
+
+  const data = await fetch(upcommingUrl);
+  const response = await data.json();
+
+  buildUpcommingCondition(response, section, symbol);
+}
+
+// Func that builds the current condition section
+function buildCurrentCondition(location, section, symbol) {
+  section.innerHTML = "";
+  section.appendChild(createNewElement("div", "label", "Current conditions"));
 
   let div = createNewElement("div", "forecasts");
+
   div.appendChild(
-    createNewElement("span", "condition symbol", symbol[location.forecast.condition])
+    createNewElement(
+      "span",
+      "condition symbol",
+      symbol[location.forecast.condition]
+    )
   );
-  let span = createNewElement("span", "condition")
+  let span = createNewElement("span", "condition");
   span.appendChild(createNewElement("span", "forecast-data", location.name));
-  
-    span.appendChild(
-      createNewElement(
-        "span",
-        "forecast-data",
-        `${location.forecast.high}${symbol.degrees}/${location.forecast.low}${symbol.degrees}`
-      )
-    );
+
+  span.appendChild(
+    createNewElement(
+      "span",
+      "forecast-data",
+      `${location.forecast.high}${symbol.degrees}/${location.forecast.low}${symbol.degrees}`
+    )
+  );
   span.appendChild(
     createNewElement("span", "forecast-data", location.forecast.condition)
   );
 
-//   TODO find a way to clear the section
   div.appendChild(span);
+  section.appendChild(div);
+}
+
+// Func that builds the current condition section
+function buildUpcommingCondition(location, section, symbol) {
+  section.innerHTML = "";
+  section.appendChild(createNewElement("div", "label", "Three-day forecast"));
+
+  let div = createNewElement("div", "forecasts-info");
+
+  location.forecast.forEach((f) => {
+    let span = createNewElement("span", "upcoming");
+    span.appendChild(createNewElement("span", "symbol", symbol[f.condition]));
+    span.appendChild(
+      createNewElement(
+        "span",
+        "forecasts-data",
+        `${f.low}${symbol.degrees}/${f.high}${symbol.degrees}`
+      )
+    );
+    span.appendChild(createNewElement("span", "forecasts-data", f.condition));
+    div.appendChild(span);
+  });
+
   section.appendChild(div);
 }
