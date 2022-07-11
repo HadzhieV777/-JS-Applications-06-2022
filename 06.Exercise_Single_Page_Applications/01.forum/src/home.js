30de87fa3aeb50ccc5c6e7b7dcf7f16062f53368
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-11 23:22:45 +0300

    Edited home.js

---
+++ 06.Exercise_Single_Page_Applications/01.forum/src/home.js
@@ -0,0 +1,10 @@
+import { showDetails } from "./details.js";
+
+const section = document.getElementById("homeView");
+section.querySelector("div.topic-title").addEventListener("click", showDetails);
+section.remove();
+
+export function showHome(ev) {
+  ev.preventDefault();
+  document.querySelector("main").replaceChildren(section);
+}
