740cd77ff4f728f16e25b0506384211536e1f080
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-12 12:21:55 +0300

    Edited details.js

--- 06.Exercise_Single_Page_Applications/01.forum/src/details.js
+++ 06.Exercise_Single_Page_Applications/01.forum/src/details.js
@@ -2,7 +2,16 @@ const section = document.getElementById("detailsView");
 section.remove();
 
 export function showDetails(ev) {
-  if (ev.target.tagName == "A") {
+  let target = ev.target;
+
+  // necessary to check if ev.target is h2 because inside
+  // the a there is a h2
+  if (target.tagName == "H2") {
+    target = target.parentElement;
+  }
+
+  if (target.tagName == "A") {
+    ev.preventDefault();
     showPost();
   }
 }
