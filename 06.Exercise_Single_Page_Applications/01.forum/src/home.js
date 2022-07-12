ff2cb575911d054b876588ae3ebf129ed32b453d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-12 12:22:17 +0300

    Edited home.js

--- 06.Exercise_Single_Page_Applications/01.forum/src/home.js
+++ 06.Exercise_Single_Page_Applications/01.forum/src/home.js
@@ -2,9 +2,21 @@ import { showDetails } from "./details.js";
 
 const section = document.getElementById("homeView");
 section.querySelector("div.topic-title").addEventListener("click", showDetails);
+const form = section.querySelector("form");
+form.addEventListener("submit", onSubmit);
+
 section.remove();
 
 export function showHome(ev) {
-  ev.preventDefault();
+  // Check if there is ev, 'ev && ev === ev?'
+  ev?.preventDefault();
   document.querySelector("main").replaceChildren(section);
+}
+
+async function onSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  console.log(...formData.entries())
 }
