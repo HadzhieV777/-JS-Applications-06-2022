9918aed3cd9fe12f24dca2330edd06bc6391bd71
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-14 14:17:59 +0300

    Edited app.js

--- 07.Architecture_and_Testing/03.spaDemo-Refactoring/src/app.js
+++ 07.Architecture_and_Testing/03.spaDemo-Refactoring/src/app.js
@@ -1,35 +1,50 @@
-import { showHome } from './home.js';
-import { showCatalog } from './catalog.js';
-import { showAbout } from './about.js';
-import { showLogin } from './login.js';
-import { showRegister } from './register.js';
-import { checkUserNav, onLogout } from './util.js';
-import { showCreate } from './create.js';
+import { showHome } from "./home.js";
+import { showCatalog } from "./catalog.js";
+import { showAbout } from "./about.js";
+import { showLogin } from "./login.js";
+import { showRegister } from "./register.js";
+import { checkUserNav, onLogout } from "./util.js";
+import { showCreate } from "./create.js";
+import { render } from "./dom.js";
 
+document.querySelector("nav").addEventListener("click", onNavigate);
 
-document.querySelector('nav').addEventListener('click', onNavigate);
-document.getElementById('logoutBtn').addEventListener('click', onLogout);
 checkUserNav();
 
-// Start application in home view
-showHome();
-
 const sections = {
-    'homeBtn': showHome,
-    'catalogBtn': showCatalog,
-    'aboutBtn': showAbout,
-    'loginBtn': showLogin,
-    'registerBtn': showRegister,
-    'createBtn': showCreate,
+  homeBtn: showHome,
+  catalogBtn: showCatalog,
+  aboutBtn: showAbout,
+  loginBtn: showLogin,
+  registerBtn: showRegister,
+  createBtn: showCreate,
+  logoutBtn: onLogout,
 };
 
-function onNavigate(event) {
-    if (event.target.tagName == 'A') {
-        const view = sections[event.target.id];
+// Start application in home view
+goTo('homeBtn');
 
-        if (typeof view == 'function') {
-            event.preventDefault();
-            view();
-        }
+function onNavigate(event) {
+  if (event.target.tagName == "A") {
+    const viewName = event.target.id;
+    if (goTo(viewName)) {
+      event.preventDefault();
     }
+  }
+}
+
+function goTo(viewName) {
+  const view = sections[viewName];
+
+  if (typeof view == "function") {
+    view({
+      render,
+      goTo,
+      checkUserNav
+    });
+
+    return true;
+  } else {
+    return false;
+  }
 }
