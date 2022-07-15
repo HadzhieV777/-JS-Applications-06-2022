1e7504acb9c934a59582cb72173134a1c07dd39a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-15 14:37:23 +0300

    Edited register.js

--- 07.Architecture_and_Testing/03.spaDemo-Refactoring/src/register.js
+++ 07.Architecture_and_Testing/03.spaDemo-Refactoring/src/register.js
@@ -1,24 +1,18 @@
-import { showHome } from "./home.js";
-import { checkUserNav } from "./util.js";
-import { request } from "./api.js";
+import { post } from "./api.js";
+import { createSubmitHandler } from "./util.js";
 
 const section = document.getElementById("registerView");
 const form = section.querySelector("form");
-form.addEventListener("submit", onSubmit);
+createSubmitHandler(form, onSubmit);
 section.remove();
+let ctx = null;
 
-export function showRegister() {
-  document.querySelector("main").replaceChildren(section);
+export function showRegister(inCtx) {
+  ctx = inCtx;
+  ctx.render(section);
 }
 
-async function onSubmit(event) {
-  event.preventDefault();
-  const formData = new FormData(form);
-
-  const email = formData.get("email").trim();
-  const password = formData.get("password").trim();
-  const repass = formData.get("repass").trim();
-
+async function onSubmit({ email, password, repass }) {
   if (email == "" || password == "") {
     return alert("All fields are required!");
   }
@@ -26,16 +20,16 @@ async function onSubmit(event) {
     return alert("Passwords don't match!");
   }
 
-  const { accessToken, _id } = await request(
-    "/users/register",
-    { email, password }
-  );
+  const { accessToken, _id } = await post("/users/register", {
+    email,
+    password,
+  });
   const userData = {
     email,
     accessToken,
     id: _id,
   };
   sessionStorage.setItem("userData", JSON.stringify(userData));
-  checkUserNav();
-  showHome();
+  ctx.checkUserNav;
+  ctx.goTo("homeBtn");
 }
