63c3296c8123967733aa3d8d5f3c3330f064de26
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-15 14:37:45 +0300

    Edited util.js

--- 07.Architecture_and_Testing/03.spaDemo-Refactoring/src/util.js
+++ 07.Architecture_and_Testing/03.spaDemo-Refactoring/src/util.js
@@ -1,27 +1,32 @@
-import { showHome } from './home.js';
+import { get } from "./api.js";
 
 export function checkUserNav() {
-    const userData = JSON.parse(sessionStorage.getItem('userData'));
-    if (userData != null) {
-        document.getElementById('greeting').textContent = `Welcome, ${userData.email}!`;
-        document.getElementById('userNav').style.display = 'inline-block';
-        document.getElementById('guestNav').style.display = 'none';
-    } else {
-        document.getElementById('userNav').style.display = 'none';
-        document.getElementById('guestNav').style.display = 'inline-block';
-    }
+  const userData = JSON.parse(sessionStorage.getItem("userData"));
+  if (userData != null) {
+    document.getElementById(
+      "greeting"
+    ).textContent = `Welcome, ${userData.email}!`;
+    document.getElementById("userNav").style.display = "inline-block";
+    document.getElementById("guestNav").style.display = "none";
+  } else {
+    document.getElementById("userNav").style.display = "none";
+    document.getElementById("guestNav").style.display = "inline-block";
+  }
 }
 
-export function onLogout() {
-    const userData = JSON.parse(sessionStorage.getItem('userData'));
+export function onLogout(ctx) {
+  get("/users/logout");
+  sessionStorage.removeItem("userData");
+  ctx.checkUserNav();
+  ctx.goTo("homeBtn");
+}
 
-    fetch('http://localhost:3030/users/logout', {
-        method: 'get',
-        headers: {
-            'X-Authorization': userData.accessToken
-        }
-    });
-    sessionStorage.removeItem('userData');
-    checkUserNav();
-    showHome();
+export function createSubmitHandler(form, callback) {
+  form.addEventListener("submit", onSubmit);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    callback(Object.fromEntries([...formData.entries()]));
+  }
 }
