e181c2c68ab0e499c5b395f843ce0eaa0a38f8e7
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-17 14:12:43 +0300

    Edited catalog.js

--- 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/views/catalog.js
+++ 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/views/catalog.js
@@ -1,5 +1,13 @@
+import { getAllIdeas } from "../api/data.js";
+
 const section = document.getElementById("dashboard-holder");
 
-export function showCatalog(context) {
+export async function showCatalog(context) {
   context.showSection(section);
+
+  const ideas = await getAllIdeas();
+}
+
+function createIdeaPreview(idea) {
+  
 }
