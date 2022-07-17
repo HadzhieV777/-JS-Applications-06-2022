c492fff58f0cc70ff7881361debea14da29cf633
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-17 13:23:52 +0300

    Edited app.js

--- 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/app.js
+++ 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/app.js
@@ -1,23 +1,45 @@
-// import * as api from "./api/api.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+import { showHome } from "./views/homepage.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
 
 const main = document.querySelector("main");
+document.getElementById("views").remove();
 
-const homeView = document.getElementById("homeView");
-const registerView = document.getElementById("registerView");
-const loginView = document.getElementById("loginView");
-const catalogView = document.getElementById("dashboard-holder");
-const detailsView = document.getElementById("detailsView");
-const createView = document.getElementById("createView");
+document.querySelector("nav").addEventListener("click", onNavigate);
 
-const paths = {
-  "/": homeView,
-  "/register": registerView,
-  "/login": loginView,
-  "/catalog": catalogView,
-  "/details": detailsView,
-  "/create": createView,
+const urls = {
+  "/": showHome,
+  "/register": showRegister,
+  "/login": showLogin,
+  "/catalog": showCatalog,
+  "/details": showDetails,
+  "/create": showCreate,
 };
 
-window.showSection = () => {
-  main.appendChild(homeView);
+const context = {
+  showSection,
 };
+
+function showSection(section) {
+  main.replaceChildren(section);
+}
+
+function onNavigate(event) {
+  let target = event.target;
+  if (target.tagName == "IMG") {
+    target = target.parentElement;
+  }
+
+  if (target.tagName == "A") {
+    event.preventDefault();
+    const url = new URL(target.href);
+    const handler = urls[url.pathname];
+    
+    if (typeof handler == "function") {
+      handler(context);
+    }
+  }
+}
