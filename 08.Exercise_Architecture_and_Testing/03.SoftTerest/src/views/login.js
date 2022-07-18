b7348356dde009008a1343bb3aeb1b931a177be5
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-18 10:40:45 +0300

    Edited login.js

--- 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/views/login.js
+++ 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/views/login.js
@@ -1,5 +1,27 @@
+import { login } from "../api/users.js";
+
 const section = document.getElementById("loginView");
+const form = section.querySelector("form");
+form.addEventListener("submit", onSubmit);
+
+let ctx = null;
 
 export function showLogin(context) {
+  // when showLogin, assign context to ctx, to be available for onSubmit
+  ctx = context;
   context.showSection(section);
+}
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  await login(email, password);
+  
+  form.reset();
+  ctx.updateNav();
+  ctx.goToPage("/");
 }
