0859aa0c2fbe66e0f3677d7a505461b16865029b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-18 10:41:03 +0300

    Edited register.js

--- 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/views/register.js
+++ 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/views/register.js
@@ -1,5 +1,27 @@
+import { register } from "../api/users.js";
+
 const section = document.getElementById("registerView");
+const form = section.querySelector("form");
+form.addEventListener("submit", onSubmit);
+
+let ctx = null;
 
 export function showRegister(context) {
+  ctx = context;
   context.showSection(section);
+}
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const repass = formData.get("repeatPassword");
+
+  await register(email, password, repass);
+  
+  form.reset();
+  ctx.updateNav();
+  ctx.goToPage("/");
 }
