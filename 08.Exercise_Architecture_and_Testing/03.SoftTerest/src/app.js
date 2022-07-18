cad5f8429691e9609a8ed033ad760090c549559b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-18 10:38:57 +0300

    Edited app.js

--- 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/app.js
+++ 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/app.js
@@ -5,6 +5,7 @@ import { showHome } from "./views/homepage.js";
 import { showLogin } from "./views/login.js";
 import { showRegister } from "./views/register.js";
 import { initialize } from "./router.js";
+import { logout } from "./api/users.js";
 
 document.getElementById("views").remove();
 
@@ -15,9 +16,18 @@ const urls = {
   "/catalog": showCatalog,
   "/details": showDetails,
   "/create": showCreate,
+  '/logout': onLogout,
 };
 
 const router = initialize(urls);
+router.updateNav();
 
 // Start app in home view
 router.goToPage("/");
+
+
+function onLogout() {
+  logout()
+  router.updateNav()
+  router.goToPage('/')
+}
