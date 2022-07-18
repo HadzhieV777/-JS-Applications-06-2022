e4fb789fafe7b0f70ef2f2b95ca1f867ae297159
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-18 10:38:39 +0300

    Edited data.js

--- 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/api/data.js
+++ 08.Exercise_Architecture_and_Testing/03.SoftTerest/src/api/data.js
@@ -2,8 +2,22 @@ import * as api from "./api.js";
 
 const endpoints = {
   ideas: "/data/ideas?select=_id%2Ctitle%2Cimg&sortBy=_createdOn%20desc",
+  singleIdea: "/data/ideas/",
+  create: "/data/ideas",
 };
 
 export async function getAllIdeas() {
   return api.get(endpoints.ideas);
+}
+
+export async function getById(id) {
+  return api.get(endpoints.singleIdea + id)
+}
+
+export async function createIdea(ideaData) {
+  return api.post(endpoints.create, ideaData);
+}
+
+export async function deleteById(id) {
+  return api.delete(endpoints.singleIdea + id);
 }
