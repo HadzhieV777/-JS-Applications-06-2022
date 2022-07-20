6a34e1e9e523b527567ac3d61a86dc4eb19b5786
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-20 13:18:40 +0300

    Edited search.js

--- 10.Exercise_Client_Side_Rendering/03.Search-in-List/search.js
+++ 10.Exercise_Client_Side_Rendering/03.Search-in-List/search.js
@@ -1,3 +1,31 @@
-function search() {
-   // TODO
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+import { towns } from './towns.js';
+
+const townsContainer = document.getElementById('towns');
+const input = document.getElementById('searchText');
+const button = document.querySelector('button');
+const result = document.getElementById('result');
+
+const townTemplate = (towns) => html`
+<ul>
+   ${towns.map(town => html`<li>${town}</li>`)}
+</ul>
+`;
+
+render(townTemplate(towns), townsContainer);
+
+function search(e) {
+  if (input.value != '') {
+   const list = [...townsContainer.querySelectorAll('li')];
+   list.forEach(t => t.className = '');
+   result.textContent = '';
+
+   const matches = list.filter(e => e.textContent.toLowerCase().includes(input.value.toLowerCase()));
+   matches.forEach(m => m.className = 'active');
+   result.textContent = `${matches.length} matches found`;
+   // input.value = ''
+  }
 }
+
+button.addEventListener('click', search);
