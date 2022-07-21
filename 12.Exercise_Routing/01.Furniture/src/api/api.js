83ed5fa52f5a684776d1e65289e7caf458981bf6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-21 20:44:42 +0300

    Added src dir

---
+++ 12.Exercise_Routing/01.Furniture/src/api/api.js
@@ -0,0 +1,26 @@
+import { getUserData, setUserData, clearUserData } from "../utils.js";
+
+const host = "http://localhost:3030";
+
+async function request(url, options) {
+  try {
+    const response = await fetch(host + url, options);
+
+    if (response.ok != true) {
+      if (response.status == 403) {
+        clearUserData();
+      }
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+
+    if (response.status == 204) {
+      return response;
+    } else {
+      return response.json();
+    }
+  } catch (err) {
+    alert(err.message);
+    throw err;
+  }
+}
---
+++ 12.Exercise_Routing/01.Furniture/src/lib.js
@@ -0,0 +1,5 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { until } from "../node_modules/lit-html/directives/until.js";
+import page from "../node_modules/page/page.mjs"; // .mjs because will be used as module not as script
+
+export { html, render, until, page };
