177d7c5d3c8b4c7f87c2ebf175c496137dd6c8f0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-21 22:49:26 +0300

    Edited api.js

--- 12.Exercise_Routing/01.Furniture/src/api/api.js
+++ 12.Exercise_Routing/01.Furniture/src/api/api.js
@@ -24,3 +24,68 @@ async function request(url, options) {
     throw err;
   }
 }
+
+function getOptions(method = "get", body) {
+  const options = {
+    method,
+    headers: {},
+  };
+
+  const token = sessionStorage.getItem("authToken");
+  if (token != null) {
+    options.headers["X-Authorization"] = token;
+  }
+
+  if (body) {
+    options.headers["Content-type"] = "application/json";
+    options.body = JSON.stringify(body);
+  }
+
+  return options;
+}
+
+export async function get(url) {
+  return await request(url, getOptions())
+}
+
+export async function post(url, data) {
+  return await request(url, getOptions('post', data))
+}
+
+export async function put(url) {
+  return await request(url, getOptions('put', data))
+}
+
+export async function del(url) {
+  return await request(url, getOptions('delete'))
+}
+
+export async function login(email, password) {
+  const result = await post(host + "/users/login", { email, password });
+
+  sessionStorage.setItem("email", result.email);
+  sessionStorage.setItem("authToken", result.accessToken);
+  sessionStorage.setItem("userId", result._id);
+
+  return result;
+}
+
+export async function register(email, password) {
+  const result = await post(host + "/users/register", { email, password });
+
+  sessionStorage.setItem("email", result.email);
+  sessionStorage.setItem("authToken", result.accessToken);
+  sessionStorage.setItem("userId", result._id);
+
+  return result;
+}
+
+export async function logout() {
+  const result = await get(host + "/users/logout");
+
+  sessionStorage.remove("email");
+  sessionStorage.remove("authToken");
+  sessionStorage.remove("userId");
+
+  return result;
+}
