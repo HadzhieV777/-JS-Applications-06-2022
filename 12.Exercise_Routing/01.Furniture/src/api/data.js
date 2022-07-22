4f33862c49fcfe3a262f2e4ab79cdcd1e66f93f0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-22 23:03:03 +0300

    Edited data.js

--- 12.Exercise_Routing/01.Furniture/src/api/data.js
+++ 12.Exercise_Routing/01.Furniture/src/api/data.js
@@ -4,25 +4,42 @@ export const login = api.login;
 export const register = api.register;
 export const logout = api.logout;
 
+
+
 const endpoints = {
   all: "/data/catalog",
   byId: "/data/catalog/",
   myItems: (userId) => `/data/catalog?where=_ownerId%3D%22${userId}%22`,
+  create: "/data/catalog",
+  edit: "/data/catalog/",
+  delete: "/data/catalog/",
 };
-
 
 // GET
 export async function getAll() {
-  return api.get(endpoints.all);
+  return api.get(api.host + endpoints.all);
 }
 
 export async function getById(id) {
-  return api.get(endpoints.byId + id);
+  return api.get(api.host + endpoints.byId + id);
 }
 
 export async function getMyItems(userId) {
-  return api.get(endpoints.myItems(userId));
+  return api.get(api.host + endpoints.myItems(userId));
 }
 
 // CREATE
-// 40:13 min
+export async function createItem(data) {
+  return api.post(api.host + endpoints.create, data);
+}
+
+export async function editItem(id, data) {
+  return api.put(api.host + endpoints.edit + id, data);
+}
+
+export async function deleteItem(id) {
+  return api.del(api.host + endpoints.delete + id)
+}
+
+
+// 45: 45
