403f19e7b0fb2cc403e0afdd83dcbbac6ff8ceba
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-24 11:56:37 +0300

    Edited api.js

--- 12.Exercise_Routing/01.Furniture/src/api/api.js
+++ 12.Exercise_Routing/01.Furniture/src/api/api.js
@@ -31,61 +31,58 @@ function getOptions(method = "get", body) {
     headers: {},
   };
 
-  const token = sessionStorage.getItem("authToken");
-  if (token != null) {
-    options.headers["X-Authorization"] = token;
-  }
-
   if (body) {
     options.headers["Content-type"] = "application/json";
     options.body = JSON.stringify(body);
+  }
+
+  const userData = getUserData();
+  if (userData != null) {
+    options.headers["X-Authorization"] = userData.token;
   }
 
   return options;
 }
 
 export async function get(url) {
-  return await request(url, getOptions())
+  return request(url, getOptions());
 }
 
 export async function post(url, data) {
-  return await request(url, getOptions('post', data))
+  return request(url, getOptions("post", data));
 }
 
 export async function put(url, data) {
-  return await request(url, getOptions('put', data))
+  return request(url, getOptions("put", data));
 }
 
 export async function del(url) {
-  return await request(url, getOptions('delete'))
+  return request(url, getOptions("delete"));
 }
 
 export async function login(email, password) {
   const result = await post(host + "/users/login", { email, password });
 
-  sessionStorage.setItem("email", result.email);
-  sessionStorage.setItem("authToken", result.accessToken);
-  sessionStorage.setItem("userId", result._id);
-
-  return result;
+  const userData = {
+    email: result.email,
+    id: result._id,
+    token: result.accessToken,
+  };
+  setUserData(userData);
 }
 
 export async function register(email, password) {
   const result = await post(host + "/users/register", { email, password });
 
-  sessionStorage.setItem("email", result.email);
-  sessionStorage.setItem("authToken", result.accessToken);
-  sessionStorage.setItem("userId", result._id);
-
-  return result;
+  const userData = {
+    email: result.email,
+    id: result._id,
+    token: result.accessToken,
+  };
+  setUserData(userData);
 }
 
 export async function logout() {
-  const result = await get(host + "/users/logout");
-
-  sessionStorage.removeItem("email");
-  sessionStorage.removeItem("authToken");
-  sessionStorage.removeItem("userId");
-
-  return result;
+  await get(host + "/users/logout");
+  clearUserData();
 }
