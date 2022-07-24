27476c96bfc7683d46e8d8e139055320066355fb
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-24 13:54:11 +0300

    login.js

--- 12.Exercise_Routing/01.Furniture/src/views/login.js
+++ 12.Exercise_Routing/01.Furniture/src/views/login.js
@@ -1,7 +1,7 @@
 import { login } from "../api/data.js";
 import { html } from "../lib.js";
 
-const loginTemplate = (onSubmit) => html` <div class="row space-top">
+const loginTemplate = (onSubmit, errorMsg) => html` <div class="row space-top">
     <div class="col-md-12">
       <h1>Login User</h1>
       <p>Please fill all fields.</p>
@@ -10,14 +10,22 @@ const loginTemplate = (onSubmit) => html` <div class="row space-top">
   <form @submit=${onSubmit}>
     <div class="row space-top">
       <div class="col-md-4">
+        ${errorMsg
+          ? html`<div class="form-group error">${errorMsg}</div>`
+          : null}
         <div class="form-group">
           <label class="form-control-label" for="email">Email</label>
-          <input class="form-control" id="email" type="text" name="email" />
+          <input
+            class=${"form-control" + (errorMsg ? " is-invalid" : "")}
+            id="email"
+            type="text"
+            name="email"
+          />
         </div>
         <div class="form-group">
           <label class="form-control-label" for="password">Password</label>
           <input
-            class="form-control"
+            class=${"form-control" + (errorMsg ? " is-invalid" : "")}
             id="password"
             type="password"
             name="password"
@@ -29,17 +37,26 @@ const loginTemplate = (onSubmit) => html` <div class="row space-top">
   </form>`;
 
 export function loginPage(ctx) {
-  ctx.render(loginTemplate(onSubmit));
+  update()
+
+  function update(errorMsg) {
+    ctx.render(loginTemplate(onSubmit, errorMsg));
+  }
 
   // onSubmit func is inside the loginPage func because we need to use ctx to redirect
   async function onSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
 
-    const email = formData.get("email");
-    const password = formData.get("password");
+    const email = formData.get("email").trim();
+    const password = formData.get("password").trim();
 
-    await login(email, password);
-    ctx.page.redirect("/");
+    try {
+      await login(email, password);
+      ctx.updateUserNav();
+      ctx.page.redirect("/");
+    } catch (error) {
+      update(error.message)
+    }
   }
 }
