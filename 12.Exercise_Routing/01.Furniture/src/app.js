454c2b871dd4f27c1dd1d6df6637a39d80359f55
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-24 13:53:58 +0300

    Edited app.js

--- 12.Exercise_Routing/01.Furniture/src/app.js
+++ 12.Exercise_Routing/01.Furniture/src/app.js
@@ -1,4 +1,6 @@
+import { logout } from "./api/api.js";
 import { page, render } from "./lib.js";
+import { getUserData } from "./utils.js";
 import { catalogPage } from "./views/catalog.js";
 import { createPage } from "./views/create.js";
 import { detailsPage } from "./views/details.js";
@@ -6,8 +8,8 @@ import { editPage } from "./views/edit.js";
 import { loginPage } from "./views/login.js";
 import { registerPage } from "./views/register.js";
 
-
 const root = document.querySelector("div.container");
+document.getElementById("logoutBtn").addEventListener("click", onLogout);
 
 page(decorateContext); // This will run before each page, meaning every page bellow will have the context
 page("/", catalogPage);
@@ -18,10 +20,12 @@ page("/login", loginPage);
 page("/register", registerPage);
 page("/my-furniture", catalogPage);
 
+updateUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
   ctx.render = (content) => render(content, root);
+  ctx.updateUserNav = updateUserNav;
 
   //  if (!next()) this middleware will block the rest (if any other middlewares)
   next();
@@ -37,3 +41,21 @@ function decorateContext(ctx, next) {
 - Simply middleware is a function that can only be applied using routes. 
 - We can access and modify request and response data using middleware.
 */
+
+function updateUserNav() {
+  const userData = getUserData();
+
+  if (userData) {
+    document.getElementById("user").style.display = "inline-block";
+    document.getElementById("guest").style.display = "none";
+  } else {
+    document.getElementById("user").style.display = "none";
+    document.getElementById("guest").style.display = "inline-block";
+  }
+}
+
+async function onLogout() {
+  await logout();
+  updateUserNav()
+  page.redirect("/");
+}
