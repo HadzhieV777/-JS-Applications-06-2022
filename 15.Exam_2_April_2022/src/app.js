c94a5a7d2cd5a9734d736dd6fb9d0383d46ae513
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-28 10:47:28 +0300

    Edited app.js

--- 15.Exam_2_April_2022/src/app.js
+++ 15.Exam_2_April_2022/src/app.js
@@ -4,32 +4,65 @@ import { dashboardView } from "./views/dashboard.js";
 import { editView } from "./views/edit.js";
 import { homeView } from "./views/home.js";
 import { loginView } from "./views/login.js";
-import { navigationView } from "./views/nav.js";
 import { registerView } from "./views/register.js";
+import { getUserData } from "./utils.js";
+import { logout } from "./api/api.js";
+import { detailsView } from "./views/details.js";
 
-const headerElement = document.querySelector("header");
-const root = document.getElementById("content");
+const root = document.querySelector("#content");
 
+document.getElementById("logoutBtn").addEventListener("click", onLogout);
+
+const routes = {
+  home: "/",
+  login: "/login",
+  edit: "/edit",
+  dashboard: "/dashboard",
+  details: "/details/:id",
+  register: "/register",
+  create: "/create-postcard",
+};
 
 page(decorateContext);
-page("/", homeView);
-page("/login", loginView);
-page("/edit", editView);
-page("/dashboard", dashboardView);
-page("/register", registerView);
-page("/create", createView);
-//  logout
+page(routes.home, homeView);
+page(routes.login, loginView);
+page(routes.edit, editView);
+page(routes.dashboard, dashboardView);
+page(routes.details, detailsView);
+page(routes.register, registerView);
+page(routes.create, createView);
 
-updateUserNav();
+setUserNav();
 page.start();
 
 function decorateContext(ctx, next) {
   ctx.render = (content) => render(content, root);
-  ctx.updateUserNav = updateUserNav
+  ctx.setUserNav = setUserNav;
 
   next();
 }
 
-function updateUserNav() {
-  render(navigationView(), headerElement);
+function setUserNav() {
+  const user = getUserData();
+  if (user) {
+    document
+      .querySelectorAll(".user")
+      .forEach((x) => (x.style.display = "inline"));
+    document
+      .querySelectorAll(".guest")
+      .forEach((x) => (x.style.display = "none"));
+  } else {
+    document
+      .querySelectorAll(".user")
+      .forEach((x) => (x.style.display = "none"));
+    document
+      .querySelectorAll(".guest")
+      .forEach((x) => (x.style.display = "inline"));
+  }
+}
+
+async function onLogout() {
+  await logout();
+  setUserNav();
+  page.redirect("/");
 }
