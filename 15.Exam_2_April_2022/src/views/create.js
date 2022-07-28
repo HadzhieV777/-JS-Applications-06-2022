e11c5366928fd3570f53c722c13495a88948e3f0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-28 10:48:00 +0300

    Edited create.js

--- 15.Exam_2_April_2022/src/views/create.js
+++ 15.Exam_2_April_2022/src/views/create.js
@@ -1,6 +1,8 @@
 import { html } from "../lib.js";
+import { createPet } from "../api/data.js";
 
-const createTemplate = () => html`<section id="createPage">
+const createTemplate = (onSubmit) => html`<section id="createPage">
+<form class="createForm" @submit=${onSubmit}>
   <form class="createForm">
     <img src="./images/cat-create.jpg" />
     <div>
@@ -36,5 +38,36 @@ const createTemplate = () => html`<section id="createPage">
 </section>`;
 
 export function createView(ctx) {
-  ctx.render(createTemplate());
+  ctx.render(createTemplate(onSubmit));
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const pet = {
+      name: formData.get("name").trim(),
+      breed: formData.get("breed").trim(),
+      age: formData.get("age").trim(),
+      weight: formData.get("weight").trim(),
+      image: formData.get("image").trim(),
+    };
+
+    if (Object.values(pet).some((x) => !x)) {
+      return alert("All fields are required!");
+    }
+
+    await createPet(pet);
+    event.target.reset();
+    ctx.page.redirect("/dashboard");
+  }
 }
+
+/* 
+The some() method checks if any array elements pass a test (provided as a callback function).
+The some() method executes the callback function once for each array element.
+The some() method returns true (and stops) if the function returns true for one of the array elements.
+The some() method returns false if the function returns false for all of the array elements.
+The some() method does not execute the function for empty array elements.
+The some() method does not change the original array.
+*/
