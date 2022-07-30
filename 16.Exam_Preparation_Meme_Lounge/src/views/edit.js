d856a8cba9f277ee55bf3aef10e8fd9ea14309a5
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-30 14:52:17 +0300

    Edited edit.js

--- 16.Exam_Preparation_Meme_Lounge/src/views/edit.js
+++ 16.Exam_Preparation_Meme_Lounge/src/views/edit.js
@@ -1,37 +1,60 @@
+import { getMemeById, updateMeme } from "../api/memes.js";
 import { html } from "../lib.js";
+import { notify } from "../notify.js";
 
-const editTemplate = () => html` <section id="edit-meme">
-  <form id="edit-form">
+const editTemplate = (meme, onSubmit) => html` <section id="edit-meme">
+  <form @submit=${onSubmit} id="edit-form">
     <h1>Edit Meme</h1>
     <div class="container">
       <label for="title">Title</label>
-      <input id="title" type="text" placeholder="Enter Title" name="title" />
+      <input
+        id="title"
+        type="text"
+        placeholder="Enter Title"
+        name="title"
+        .value="${meme.title}"
+      />
       <label for="description">Description</label>
       <textarea
         id="description"
         placeholder="Enter Description"
         name="description"
-      >
-                    Programming is often touted as a smart and lucrative career path.
-                    It's a job that (sometimes) offers flexibility and great benefits.
-                    But it's far from sunshine and Nyan Cat rainbows. The hours are long.
-                    The mistakes are frustrating. And your eyesight is almost guaranteed to suffer.
-                    These memes cover most of the frustration (and funny moments) of programming.
-                    At least we can laugh through the pain. 
-                </textarea
-      >
+        .value="${meme.description}"
+      ></textarea>
       <label for="imageUrl">Image Url</label>
       <input
         id="imageUrl"
         type="text"
         placeholder="Enter Meme ImageUrl"
         name="imageUrl"
+        .value="${meme.imageUrl}"
       />
       <input type="submit" class="registerbtn button" value="Edit Meme" />
     </div>
   </form>
 </section>`;
 
-export function editView(ctx) {
-  ctx.render(editTemplate());
+export async function editView(ctx) {
+  const meme = await getMemeById(ctx.params.id);
+
+  ctx.render(editTemplate(meme, onSubmit));
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const meme = {
+      title: formData.get("title"),
+      description: formData.get("description"),
+      imageUrl: formData.get("imageUrl"),
+    };
+
+    if (meme.title == "" || meme.description == "" || meme.imageUrl == "") {
+      return notify("All fields are required!");
+    }
+
+    await updateMeme(ctx.params.id, meme);
+    event.target.reset();
+    ctx.page.redirect("/memes/" + ctx.params.id);
+  }
 }
