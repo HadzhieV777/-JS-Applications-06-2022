dd55c00bf7aa992fd6119bdccfd2ee232794e5b3
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-30 14:53:04 +0300

    Edited profile.js

--- 16.Exam_Preparation_Meme_Lounge/src/views/profile.js
+++ 16.Exam_Preparation_Meme_Lounge/src/views/profile.js
@@ -1,36 +1,39 @@
+import { getUserMemes } from "../api/memes.js";
 import { html } from "../lib.js";
+import { getUserData } from "../utils.js";
 
-const profileTemplate = () => html` <section
+const profileTemplate = (memes, userData) => html` <section
   id="user-profile-page"
   class="user-profile"
 >
   <article class="user-info">
-    <img id="user-avatar-url" alt="user-profile" src="/images/female.png" />
+    <img
+      id="user-avatar-url"
+      alt="user-profile"
+      src="/images/${userData.gender}.png"
+    />
     <div class="user-content">
-      <p>Username: Mary</p>
-      <p>Email: mary@abv.bg</p>
-      <p>My memes count: 2</p>
+      <p>Username: ${userData.username}</p>
+      <p>Email: ${userData.email}</p>
+      <p>My memes count: ${memes.length}</p>
     </div>
   </article>
   <h1 id="user-listings-title">User Memes</h1>
   <div class="user-meme-listings">
-    <!-- Display : All created memes by this user (If any) -->
-    <div class="user-meme">
-      <p class="user-meme-title">Java Script joke</p>
-      <img class="userProfileImage" alt="meme-img" src="/images/1.png" />
-      <a class="button" href="#">Details</a>
-    </div>
-    <div class="user-meme">
-      <p class="user-meme-title">Bad code can present some problems</p>
-      <img class="userProfileImage" alt="meme-img" src="/images/3.png" />
-      <a class="button" href="#">Details</a>
-    </div>
-
-    <!-- Display : If user doesn't have own memes  -->
-    <p class="no-memes">No memes in database.</p>
+    ${memes.length == 0
+      ? html`<p class="no-memes">No memes in database.</p>`
+      : memes.map(memeCard)}
   </div>
 </section>`;
 
-export function profileView(ctx) {
-  ctx.render(profileTemplate());
+const memeCard = (meme) => html` <div class="user-meme">
+  <p class="user-meme-title">${meme.title}</p>
+  <img class="userProfileImage" alt="meme-img" src="${meme.imageUrl}" />
+  <a class="button" href="/memes/${meme._id}">Details</a>
+</div>`;
+
+export async function profileView(ctx) {
+  const userData = getUserData();
+  const memes = await getUserMemes(userData.id);
+  ctx.render(profileTemplate(memes, userData));
 }
