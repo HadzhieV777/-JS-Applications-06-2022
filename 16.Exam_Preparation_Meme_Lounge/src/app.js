c959b56da1b52545ff620799f74e7099beae7ce7
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-07-30 14:51:23 +0300

    Edited app.js

--- 16.Exam_Preparation_Meme_Lounge/src/app.js
+++ 16.Exam_Preparation_Meme_Lounge/src/app.js
@@ -7,6 +7,8 @@ import { loginView } from "./views/login.js";
 import { catalogView } from "./views/catalog.js";
 import { profileView } from "./views/profile.js";
 import { registerView } from "./views/register.js";
+import { getUserData } from "./utils.js";
+import { logout } from "./api/users.js";
 
 const routes = {
   home: "/",
@@ -20,6 +22,7 @@ const routes = {
 };
 
 const main = document.querySelector("main");
+document.getElementById("logoutBtn").addEventListener("click", onLogout);
 
 page(decorateContext);
 page(routes.home, homeView);
@@ -31,14 +34,36 @@ page(routes.profile, profileView);
 page(routes.catalog, catalogView);
 page(routes.details, detailsView);
 
+updateNav();
 page.start();
 
 function decorateContext(ctx, next) {
   ctx.render = renderMain;
+  ctx.updateNav = updateNav;
 
   next();
 }
 
 function renderMain(templateResult) {
   render(templateResult, main);
+}
+
+function updateNav() {
+  const userData = getUserData();
+  if (userData) {
+    document.querySelector(".user").style.display = "block";
+    document.querySelector(".guest").style.display = "none";
+    document.querySelector(
+      ".user span"
+    ).textContent = `Welcome, ${userData.email}`;
+  } else {
+    document.querySelector(".user").style.display = "none";
+    document.querySelector(".guest").style.display = "block";
+  }
+}
+
+function onLogout() {
+  logout();
+  updateNav();
+  page.redirect("/");
 }
