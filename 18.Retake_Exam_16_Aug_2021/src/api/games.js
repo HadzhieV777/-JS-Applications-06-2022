586b6f42f433a3f724208eb4320a245340c88684
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-02 10:15:33 +0300

    Added games.js

---
+++ 18.Retake_Exam_16_Aug_2021/src/api/games.js
@@ -0,0 +1,14 @@
+import * as api from "./api.js";
+
+const endpoints = {
+  recent: "/data/games?sortBy=_createdOn%20desc&distinct=category",
+  all: '/data/games?sortBy=_createdOn%20desc'
+};
+
+export async function getRecent() {
+  return api.get(endpoints.recent);
+}
+
+export async function getAll() {
+    return api.get(endpoints.all)
+}
