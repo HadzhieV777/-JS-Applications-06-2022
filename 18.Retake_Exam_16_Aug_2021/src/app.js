c85344e8dd7f701a586803c92e5929ab82bdd909
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-03 13:38:06 +0300

    Edited app.js

--- 18.Retake_Exam_16_Aug_2021/src/app.js
+++ 18.Retake_Exam_16_Aug_2021/src/app.js
@@ -1,5 +1,9 @@
 import { page } from "./lib.js";
+
+import { addSession } from "./middlewares/session.js";
 import { addRender } from "./middlewares/render.js";
+
+import { logout } from "./api/users.js";
 import { catalogPage } from "./views/catalog.js";
 import { createPage } from "./views/create.js";
 import { detailsPage } from "./views/details.js";
@@ -11,25 +15,34 @@ import { registerPage } from "./views/register.js";
 // import * as api from './api/api.js'
 // window.api = api
 
-
 const routes = {
   home: "/",
   catalog: "/catalog",
   login: "/login",
   register: "/register",
+  logout: "/logout",
   create: "/create",
   details: "/details/:id",
   edit: "/edit/:id",
 };
 
+// Middlewares
+page(addSession);
 page(addRender);
 
+// Pages
 page(routes.home, homePage);
 page(routes.catalog, catalogPage);
 page(routes.login, loginPage);
 page(routes.register, registerPage);
+page(routes.logout, onLogout);
 page(routes.create, createPage);
 page(routes.details, detailsPage);
 page(routes.edit, editPage);
 
 page.start();
+
+function onLogout(ctx) {
+  logout();
+  ctx.page.redirect("/");
+}
