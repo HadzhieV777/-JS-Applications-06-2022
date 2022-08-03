ac425071981fea71f13a399f4a6fc52894b4981f
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-03 20:43:56 +0300

    Edited commentForm.js

--- 18.Retake_Exam_16_Aug_2021/src/views/commentForm.js
+++ 18.Retake_Exam_16_Aug_2021/src/views/commentForm.js
@@ -2,20 +2,33 @@ import { html, nothing } from "../lib.js";
 import { createSubmitHandler } from "../util.js";
 import * as commentsService from "../api/comments.js";
 
-const formTemplate = () => html` <article class="create-comment">
+const formTemplate = (onSubmit) => html` <article class="create-comment">
   <label>Add new comment:</label>
-  <form class="form">
+  <form class="form" @submit=${onSubmit}>
     <textarea name="comment" placeholder="Comment......"></textarea>
     <input class="btn submit" type="submit" value="Add Comment" />
   </form>
 </article>`;
 
-export function commentFormView(ctx, gameId) {
-  if (ctx.user) {
-    return formTemplate();
+export function commentFormView(ctx, isOwner) {
+  if (ctx.user && !isOwner) {
+    return formTemplate(createSubmitHandler(ctx, onSubmit));
   } else {
     return nothing;
   }
 }
 
-// 3:09:32
+async function onSubmit(ctx, data, event) {
+  const gameId = ctx.params.id;
+
+  if (data.comment == "") {
+    return alert("You can't add empty comment!");
+  }
+  await commentsService.postComment({
+    gameId,
+    comment: data.comment,
+  });
+
+  event.target.reset();
+  ctx.page.redirect(`/details/${gameId}`);
+}
