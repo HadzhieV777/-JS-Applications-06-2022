93279589c8925fcd3395c1dcb07a2ccab3a97c3a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-03 13:39:09 +0300

    Edited details.js

--- 18.Retake_Exam_16_Aug_2021/src/views/details.js
+++ 18.Retake_Exam_16_Aug_2021/src/views/details.js
@@ -1,57 +1,56 @@
 import { html } from "../lib.js";
+import * as gamesService from "../api/games.js";
+import { commentsView } from "./comments.js";
 
-const detailsTemplate = () => html`<section id="game-details">
+const detailsTemplate = (game, commentsSection, onDelete) => html`<section
+  id="game-details"
+>
   <h1>Game Details</h1>
   <div class="info-section">
     <div class="game-header">
-      <img class="game-img" src="images/MineCraft.png" />
-      <h1>Bright</h1>
-      <span class="levels">MaxLevel: 4</span>
-      <p class="type">Action, Crime, Fantasy</p>
+      <img class="game-img" src="${game.imageUrl}" />
+      <h1>${game.title}</h1>
+      <span class="levels">MaxLevel: ${game.maxLevel}</span>
+      <p class="type">${game.category}</p>
     </div>
 
-    <p class="text">
-      Set in a world where fantasy creatures live side by side with humans. A
-      human cop is forced to work with an Orc to find a weapon everyone is
-      prepared to kill for. Set in a world where fantasy creatures live side by
-      side with humans. A human cop is forced to work with an Orc to find a
-      weapon everyone is prepared to kill for.
-    </p>
+    <p class="text">${game.summary}</p>
 
     <!-- Bonus ( for Guests and Users ) -->
-    <div class="details-comments">
-      <h2>Comments:</h2>
-      <ul>
-        <!-- list all comments for current game (If any) -->
-        <li class="comment">
-          <p>Content: I rate this one quite highly.</p>
-        </li>
-        <li class="comment">
-          <p>Content: The best game.</p>
-        </li>
-      </ul>
-      <!-- Display paragraph: If there are no games in the database -->
-      <p class="no-comment">No comments.</p>
-    </div>
-
-    <!-- Edit/Delete buttons ( Only for creator of this game )  -->
-    <div class="buttons">
-      <a href="#" class="button">Edit</a>
-      <a href="#" class="button">Delete</a>
-    </div>
+    ${commentsSection}
+    ${game.isOwner
+      ? html`<div class="buttons">
+          <a href="/edit/${game._id}" class="button">Edit</a>
+          <a href="javascript:void(0);" @click=${onDelete} class="button"
+            >Delete</a
+          >
+        </div>`
+      : ""}
   </div>
 
   <!-- Bonus -->
   <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) -->
-  <article class="create-comment">
-    <label>Add new comment:</label>
-    <form class="form">
-      <textarea name="comment" placeholder="Comment......"></textarea>
-      <input class="btn submit" type="submit" value="Add Comment" />
-    </form>
-  </article>
+ ${}
 </section>`;
 
-export function detailsPage(ctx) {
-  ctx.render(detailsTemplate());
+export async function detailsPage(ctx) {
+  const gameId = ctx.params.id;
+  const [game, commentsSection] = await Promise.all([
+    gamesService.getById(gameId),
+    commentsView(gameId),
+  ]);
+
+  if (ctx.user) {
+    game.isOwner = ctx.user._id == game._ownerId;
+  }
+  ctx.render(detailsTemplate(game, commentsSection, onDelete));
+
+  async function onDelete() {
+    const choice = confirm(`Are you sure you want to delete ${game.title}?`);
+
+    if (choice) {
+      await gamesService.deleteById(gameId);
+      ctx.page.redirect("/");
+    }
+  }
 }
