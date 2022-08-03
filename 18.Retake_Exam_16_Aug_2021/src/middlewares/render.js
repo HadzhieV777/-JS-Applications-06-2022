37ddd293ab5ed4cb5798f7f35378dcf1d02dbc41
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-03 13:38:35 +0300

    Edited render.js

--- 18.Retake_Exam_16_Aug_2021/src/middlewares/render.js
+++ 18.Retake_Exam_16_Aug_2021/src/middlewares/render.js
@@ -1,5 +1,22 @@
-import { render } from "../lib.js";
+import { render, html } from "../lib.js";
 
+const navTemplate = (user) => html` <h1>
+    <a class="home" href="/">GamesPlay</a>
+  </h1>
+  <nav>
+    <a href="/catalog">All games</a>
+    ${user
+      ? html` <div id="user">
+          <a href="/create">Create Game</a>
+          <a href="/logout">Logout</a>
+        </div>`
+      : html`<div id="guest">
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+        </div>`}
+  </nav>`;
+
+const header = document.querySelector(".my-header");
 const root = document.getElementById("main-content");
 
 function ctxRender(content) {
@@ -7,6 +24,7 @@ function ctxRender(content) {
 }
 
 export function addRender(ctx, next) {
+  render(navTemplate(ctx.user), header);
   ctx.render = ctxRender;
 
   next();
