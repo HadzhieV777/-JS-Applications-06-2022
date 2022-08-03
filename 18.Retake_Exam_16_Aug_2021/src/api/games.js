69541b81e6d31c14acc1120f9204bc4fdaac72d2
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-03 13:37:52 +0300

    Edited games.js

--- 18.Retake_Exam_16_Aug_2021/src/api/games.js
+++ 18.Retake_Exam_16_Aug_2021/src/api/games.js
@@ -2,7 +2,11 @@ import * as api from "./api.js";
 
 const endpoints = {
   recent: "/data/games?sortBy=_createdOn%20desc&distinct=category",
-  all: '/data/games?sortBy=_createdOn%20desc'
+  all: "/data/games?sortBy=_createdOn%20desc",
+  getById: "/data/games/",
+  create: "/data/games",
+  update: "/data/games/",
+  delete: "/data/games/",
 };
 
 export async function getRecent() {
@@ -10,5 +14,21 @@ export async function getRecent() {
 }
 
 export async function getAll() {
-    return api.get(endpoints.all)
+  return api.get(endpoints.all);
+}
+
+export async function getById(id) {
+  return api.get(endpoints.getById + id);
+}
+
+export async function create(data) {
+  return api.post(endpoints.create, data);
+}
+
+export async function update(id, data) {
+  return api.put(endpoints.update + id, data);
+}
+
+export async function deleteById(id) {
+  return api.del(endpoints.delete + id);
 }
