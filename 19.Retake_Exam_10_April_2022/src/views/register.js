fe4a17e89a360dcf43e422856b2ce7225704c9e7
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-04 11:04:35 +0300

    Added views dir

---
+++ 19.Retake_Exam_10_April_2022/src/views/register.js
@@ -0,0 +1,53 @@
+import { register } from "../api/users.js";
+import { html } from "../lib.js";
+
+const registerTemplate = (onSubmit) => html` <section
+  id="register-page"
+  class="auth"
+>
+  <form id="register" @submit=${onSubmit}>
+    <h1 class="title">Register</h1>
+
+    <article class="input-group">
+      <label for="register-email">Email: </label>
+      <input type="email" id="register-email" name="email" />
+    </article>
+
+    <article class="input-group">
+      <label for="register-password">Password: </label>
+      <input type="password" id="register-password" name="password" />
+    </article>
+
+    <article class="input-group">
+      <label for="repeat-password">Repeat Password: </label>
+      <input type="password" id="repeat-password" name="repeatPassword" />
+    </article>
+
+    <input type="submit" class="btn submit-btn" value="Register" />
+  </form>
+</section>`;
+
+export async function registerPage(ctx) {
+  ctx.render(registerTemplate(onSubmit));
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const email = formData.get("email").trim();
+    const password = formData.get("password").trim();
+    const repass = formData.get("repeatPassword").trim();
+
+    if (email == "" || password == "") {
+      return notify("All fields are required!");
+    }
+
+    if (password != repass) {
+      return notify("Passwords don't match!");
+    }
+
+    await register(email, password);
+    event.target.reset();
+    ctx.page.redirect("/");
+  }
+}
