9f798c50e40eb6a55db6dd08431801629d6273d9
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-04 14:01:32 +0300

    Edited posts.js

--- 19.Retake_Exam_10_April_2022/src/api/posts.js
+++ 19.Retake_Exam_10_April_2022/src/api/posts.js
@@ -1,13 +1,35 @@
 import { del, get, post, put } from "./api.js";
 
 const routes = {
-  allPosts: "/data/posts?sortBy=_createdOn%20desc",
-  //   createMeme: "/data/memes",
-  //   getMeme: "/data/memes/",
-  //   delMeme: "/data/memes/",
-  //   updateMeme: "/data/memes/",
+  all: "/data/posts?sortBy=_createdOn%20desc",
+  myPosts: (userId) =>
+    `/data/posts?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`,
+  create: "/data/posts",
+  getPost: "/data/posts/",
+  update: "/data/posts/",
+  delete: "/data/posts/",
 };
 
 export async function getAllPosts() {
-  return get(routes.allPosts);
+  return get(routes.all);
+}
+
+export async function getMyPosts(userId) {
+  return await get(routes.myPosts(userId));
+}
+
+export async function getById(postId) {
+  return get(routes.getPost + postId);
+}
+
+export async function createPost(singlePost) {
+  return post(routes.create, singlePost);
+}
+
+export async function updatePost(postId, singlePost) {
+  return put(routes.update + postId, singlePost);
+}
+
+export async function deletePost(postId) {
+  return del(routes.delete + postId);
 }
