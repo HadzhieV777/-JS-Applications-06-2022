98c3789a4d47d81b8e236d45aed42ac21a56e4b0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-07 11:23:22 +0300

    Edited render.js

---
+++ 21.Retake_Exam_My_Theater/src/middlewares/render.js
@@ -0,0 +1,28 @@
+import { render, html } from "../lib.js";
+
+const navTemplate = (user) => html`  <nav>
+<a href="/">Theater</a>
+<ul>
+  ${
+    user
+      ? html` <li><a href="/profile">Profile</a></li>
+          <li><a href="/create">Create Event</a></li>
+          <li><a href="/logout">Logout</a></li>`
+      : html` <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>`
+  }
+</nav>`;
+
+const header = document.querySelector(".my-header");
+const root = document.getElementById("content");
+
+function ctxRender(content) {
+  render(content, root);
+}
+
+export function addRender(ctx, next) {
+  render(navTemplate(ctx.user), header);
+  ctx.render = ctxRender;
+
+  next();
+}
