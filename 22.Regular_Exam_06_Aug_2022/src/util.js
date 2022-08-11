e0015d2dc8ea90bafbbc9a768ad4381409de256b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-08-11 12:01:06 +0300

    Added util.js

---
+++ 22.Regular_Exam_06_Aug_2022/src/util.js
@@ -0,0 +1,22 @@
+// User
+export function getUserData() {
+  return JSON.parse(sessionStorage.getItem("userData"));
+}
+
+export function setUserData(data) {
+  sessionStorage.setItem("userData", JSON.stringify(data));
+}
+
+export function clearUserData() {
+  sessionStorage.removeItem("userData");
+}
+
+// Handler
+export function createSubmitHandler(ctx, handler) {
+  return function (event) {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData(event.target));
+
+    handler(ctx, formData, event);
+  };
+}
